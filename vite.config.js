0b27b7af473b9594bb54bab4d02d58bd7ba1e1ee
Rahul0552 <dharmasoth.rahul@gmail.com>
2025-02-12 02:01:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// https://vite.dev/config/
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// before it shows this way 
+// so do some changes 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Full-Stack-Developer-Roadmap/', // 👈 Important: Update this
+});
